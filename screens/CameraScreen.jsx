8e51db149393935285a62d80de09f332d873c107
cymula <amula@cytonn.com>
2023-03-07 09:40:56 +0300

    final commit

--- screens/CameraScreen.jsx
+++ screens/CameraScreen.jsx
@@ -1,12 +1,18 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 import {RNCamera} from 'react-native-camera';
 
 function CameraScreen() {
   const camera = useRef(null);
   const [takingPicture, setTakingPicture] = useState(false);
+  const [currentLongitude, setCurrentLongitude] = useState('...');
+  const [currentLatitude, setCurrentLatitude] = useState('...');
+  const [locationStatus, setLocationStatus] = useState('');
+
   takePicture = async () => {
     if (camera && !takingPicture) {
+      getOneTimeLocation();
       let options = {
         quality: 0.85,
         fixOrientation: true,
@@ -17,7 +23,14 @@ function CameraScreen() {
 
       try {
         const data = await camera.current.takePictureAsync(options);
-        Alert.alert('Success', JSON.stringify(data));
+        Alert.alert(
+          'Success',
+          JSON.stringify({
+            image: data.uri,
+            longitude: currentLongitude,
+            latitude: currentLatitude,
+          }),
+        );
       } catch (err) {
         Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
         return;
@@ -25,6 +38,28 @@ function CameraScreen() {
         setTakingPicture(false);
       }
     }
+  };
+
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      position => {
+        setLocationStatus('You are Here');
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        //getting the Longitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        //getting the Latitude from the location json
+        setCurrentLongitude(currentLongitude);
+        //Setting state Longitude to re re-render the Longitude Text
+        setCurrentLatitude(currentLatitude);
+        //Setting state Latitude to re re-render the Longitude Text
+      },
+      error => {
+        setLocationStatus(error.message);
+      },
+      {enableHighAccuracy: false, timeout: 30000, maximumAge: 1000},
+    );
   };
 
   return (
